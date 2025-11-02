f42d974a0fd2d7c44f22a7449f3b773c721def27
David Godfrey <kinqvidda@gmail.com>
2025-11-02 02:11:24 +0100

    Completed the UI

--- app/src/main/java/com/example/randomnumbergenerator/MainActivity.kt
+++ app/src/main/java/com/example/randomnumbergenerator/MainActivity.kt
@@ -6,21 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -28,17 +32,41 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.randomnumbergenerator.ui.theme.DeepViolet
+import com.example.randomnumbergenerator.ui.theme.LightLavender
+import com.example.randomnumbergenerator.ui.theme.MediumPink
+import com.example.randomnumbergenerator.ui.theme.MediumPurple
+import com.example.randomnumbergenerator.ui.theme.PalePink
 import com.example.randomnumbergenerator.ui.theme.RandomNumberGeneratorTheme
+import com.example.randomnumbergenerator.ui.theme.White
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             RandomNumberGeneratorTheme {
-                RandomNumberGeneratorScreen()
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text("Random Number Generator") },
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = DeepViolet,
+                                titleContentColor = MaterialTheme.colorScheme.onPrimary
+                            )
+                        )
+                    }
+                ) { innerPadding ->
+                    RandomNumberGeneratorScreen(
+                        Modifier
+                            .padding(innerPadding),
+                        innerPadding
+                    )
+                }
             }
         }
     }
@@ -50,14 +78,24 @@ fun MinimumValue(
 ) {
     var minValue by remember { mutableStateOf("") }
     Card(
-        modifier = Modifier
-            .size(width = 150.dp, height = 120.dp)
+        modifier
+            .size(width = 150.dp, height = 120.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = LightLavender,
+        ),
     ) {
         TextField(
             value = minValue,
             onValueChange = { minValue = it },
             label = { Text("Minimum") },
-            singleLine = true
+            singleLine = true,
+            colors = TextFieldDefaults.colors(
+                unfocusedContainerColor = LightLavender,
+                focusedContainerColor = LightLavender,
+                unfocusedTextColor = Color.Black,
+                focusedTextColor = Color.Black,
+                cursorColor = Color.Black
+            )
         )
     }
 
@@ -68,18 +106,27 @@ fun MaximumValue(
     modifier: Modifier = Modifier
 ) {
     var maxValue by remember { mutableStateOf("") }
-Card(
-    modifier = Modifier
-        .size(width = 150.dp, height = 120.dp)
-) {
-    TextField(
-        value = maxValue,
-        onValueChange = { maxValue = it },
-        label = { Text("Maximum") },
-        singleLine = true
-    )
-}
-
+    Card(
+        modifier
+            .size(width = 150.dp, height = 120.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = LightLavender,
+        ),
+    ) {
+        TextField(
+            value = maxValue,
+            onValueChange = { maxValue = it },
+            label = { Text("Maximum") },
+            singleLine = true,
+            colors = TextFieldDefaults.colors(
+                unfocusedContainerColor = LightLavender,
+                focusedContainerColor = LightLavender,
+                unfocusedTextColor = Color.Black,
+                focusedTextColor = Color.Black,
+                cursorColor = Color.Black
+            )
+        )
+    }
 }
 
 @Composable
@@ -87,7 +134,10 @@ fun GeneratedNumber(modifier: Modifier = Modifier) {
     var generatedNumber by remember { mutableStateOf("") }
     Card(
         modifier = Modifier
-            .size(width = 350.dp, height = 250.dp)
+            .size(width = 350.dp, height = 250.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MediumPurple,
+        ),
     ) {
         Column(
             modifier = Modifier
@@ -98,32 +148,39 @@ fun GeneratedNumber(modifier: Modifier = Modifier) {
             Text(
                 text = "Generated Number",
                 style = MaterialTheme.typography.headlineMedium,
-                modifier = Modifier.padding(16.dp)
+                modifier = Modifier.padding(16.dp),
+                color = MaterialTheme.colorScheme.onPrimary
             )
             Text(
                 text = "18",
                 style = MaterialTheme.typography.headlineMedium,
-                modifier = Modifier.padding(16.dp)
+                modifier = Modifier.padding(16.dp),
+                color = MaterialTheme.colorScheme.onPrimary
             )
         }
     }
 }
 
 @Composable
-fun RandomNumberGeneratorScreen(modifier: Modifier = Modifier) {
+fun RandomNumberGeneratorScreen(
+    modifier: Modifier = Modifier,
+    paddingValues: PaddingValues
+) {
     var randomNumber by remember { mutableStateOf(0) }
     Column(
-        modifier = Modifier
+        modifier
             .fillMaxSize()
-            .padding(4.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+            .background(PalePink)
+            .padding(top = paddingValues.calculateTopPadding(), bottom = paddingValues.calculateBottomPadding()),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
     ) {
         Card(
             modifier = Modifier
-                    .size(width = 350.dp, height = 420.dp),
+                .size(width = 350.dp, height = 420.dp),
             colors = CardDefaults.cardColors(
-                containerColor = MaterialTheme.colorScheme.secondary,
+                containerColor = White,
             ),
         ) {
             Column(
@@ -131,7 +188,7 @@ fun RandomNumberGeneratorScreen(modifier: Modifier = Modifier) {
                     .padding(16.dp),
                 verticalArrangement = Arrangement.SpaceBetween,
                 horizontalAlignment = Alignment.CenterHorizontally
-            ){
+            ) {
                 Row(
                     horizontalArrangement = Arrangement.SpaceBetween,
                 ) {
@@ -143,9 +200,28 @@ fun RandomNumberGeneratorScreen(modifier: Modifier = Modifier) {
                     MaximumValue()
                 }
                 Spacer(modifier = Modifier.weight(1f))
-                    GeneratedNumber()
+                GeneratedNumber()
 
             }
+        }
+
+        Spacer(modifier = Modifier.weight(1f))
+
+
+        Button(
+            onClick = { randomNumber = (1..100).random() },
+            modifier = Modifier
+                .size(width = 350.dp, height = 50.dp),
+            shape = MaterialTheme.shapes.medium,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = DeepViolet,
+                contentColor = MaterialTheme.colorScheme.onPrimary
+            )
+        ) {
+            Text(
+                text = "Generate Random Number",
+                style = MaterialTheme.typography.headlineSmall
+            )
         }
 
     }
@@ -155,6 +231,8 @@ fun RandomNumberGeneratorScreen(modifier: Modifier = Modifier) {
 @Composable
 fun RandomNumberGeneratorPreview() {
     RandomNumberGeneratorTheme {
-        RandomNumberGeneratorScreen()
+        RandomNumberGeneratorScreen(
+            paddingValues = PaddingValues(16.dp)
+        )
     }
 }
--- app/src/main/java/com/example/randomnumbergenerator/ui/theme/Color.kt
+++ app/src/main/java/com/example/randomnumbergenerator/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//New Colors
+val PalePink = Color(0xFFFFC2E2)
+val LightLavender = Color(0xFFCAADFF)
+val DeepViolet = Color(0xFF7A31E1)
+val MediumPurple = Color(0xFFAF99FF)
+val MediumPink = Color(0xFFFF99B6)
+val White = Color(0xFFFAF7F7)
